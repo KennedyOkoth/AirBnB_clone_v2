7b6f0ab8681eb5899c3518b1b9e258e78bb3702a
KennedyOkoth <keinokoth.ok@gmail.com>
2024-07-28 19:26:11 +0300

    Completed task 3.

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Hello function
+    :return: Hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    HBNB function
+    :return: HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    C function
+    :param text:
+    :return: c + text
+    """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """
+    Python function
+    :param text:
+    :return: python + text
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
